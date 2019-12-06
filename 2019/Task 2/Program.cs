e2d55680c39ed193f58873575bfea7e9d2d2c00a
Robin_tty <muellerobin95@gmail.com>
2019-12-06 01:38:38 +0100

    Complete task 2

--- 2019/Task 2/Program.cs
+++ 2019/Task 2/Program.cs
@@ -9,14 +9,24 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             const string input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,6,23,1,23,6,27,1,13,27,31,2,13,31,35,1,5,35,39,2,39,13,43,1,10,43,47,2,13,47,51,1,6,51,55,2,55,13,59,1,59,10,63,1,63,10,67,2,10,67,71,1,6,71,75,1,10,75,79,1,79,9,83,2,83,6,87,2,87,9,91,1,5,91,95,1,6,95,99,1,99,9,103,2,10,103,107,1,107,6,111,2,9,111,115,1,5,115,119,1,10,119,123,1,2,123,127,1,127,6,0,99,2,14,0,0";
-            var result = Compute(input)[0];
+            for (var i = 0; i < 100; i++)
+            {
+                for (var j = 0; j < 100; j++)
+                {
+                    var output = Compute(input, i, j);
+                    if (output[0] != 19690720) continue;
+                    output[1] = Compute(input, i, j)[1];
+                    output[2] = Compute(input, i, j)[2];
+                    return;
+                }
+            }
         }
 
-        public static int[] Compute(string input)
+        public static int[] Compute(string command, int parameter1, int parameter2)
         {
-            var inputArr = Regex.Split(input, @"\D+").Select(int.Parse).ToArray();
-            inputArr[1] = 12;
-            inputArr[2] = 2;
+            var inputArr = Regex.Split(command, @"\D+").Select(int.Parse).ToArray();
+            inputArr[1] = parameter1;
+            inputArr[2] = parameter2;
             for (var i = 0; i < inputArr.Length; i += 4)
             {
                 if (inputArr[i] == 1)
