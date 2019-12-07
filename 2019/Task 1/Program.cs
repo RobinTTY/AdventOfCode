a3577e0861d421e95f1964f0b1e139474736f0c2
Robin_tty <muellerobin95@gmail.com>
2019-12-07 21:17:17 +0100

    Add Task 1 - Part 1

---
+++ 2019/Task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AoC_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var input = File.ReadAllLines("Input.txt").Select(int.Parse).ToArray();
+            var output = CalcFuelRequirementSpacecraft(input);
+        }
+
+        private static int CalcFuelRequirementSpacecraft(IEnumerable<int> massArr) => massArr.Sum(CalcFuelRequirementModule);
+
+        private static int CalcFuelRequirementModule(int mass) => (int)Math.Floor((double)mass / 3) - 2;
+    }
+}
