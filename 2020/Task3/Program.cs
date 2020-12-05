2779cb449d95154a41a211a013712c9f58596240
RobinTTY <muellerobin95@gmail.com>
2020-12-05 01:28:10 +0100

    Add day 3

---
+++ 2020/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parsedInput = File.ReadAllText("input.txt")
+                .Split("\n").Where(line => line.Length > 0).ToList();
+
+            Console.WriteLine($"Number of trees that were hit for Part 1: {GetNumberOfTreesHit(parsedInput, 1, 3)}");
+            var result = GetNumberOfTreesHit(parsedInput, 1, 1) * GetNumberOfTreesHit(parsedInput, 1, 3) *
+                         GetNumberOfTreesHit(parsedInput, 1, 5) * GetNumberOfTreesHit(parsedInput, 1, 7) *
+                         GetNumberOfTreesHit(parsedInput, 2, 1);
+            Console.WriteLine($"Number of trees that were hit for Part 2: {result}");
+        }
+
+        public static long GetNumberOfTreesHit(List<string> input, int rowSpeed, int colSpeed) => input.Where((line, iterator) => line[iterator / rowSpeed * colSpeed % line.Length] == '#' && iterator % rowSpeed == 0).Count();
+    }
+}
