284843fa08e895195b44295b52b1acd6fe9e9529
RobinTTY <muellerobin95@gmail.com>
2020-12-12 16:38:20 +0100

    Add day 5

--- 2020/Task5/Program.cs
+++ 2020/Task5/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2020
 {
@@ -6,7 +9,13 @@ namespace AdventOfCode2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = File.ReadAllLines("input.txt").ToList().Select(GetSeatId).ToList();
+            Console.WriteLine($"Result for part 1: {result.Max()}");
+            Console.WriteLine($"Result for part 1: {SearchMySeat(result)}");
         }
+
+        private static int GetSeatId(string seatIdentifier) => Convert.ToInt32(seatIdentifier.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1'), 2);
+
+        private static int SearchMySeat(IReadOnlyCollection<int> seatIds) => Enumerable.Range(seatIds.Min(), seatIds.Max() - seatIds.Min() + 1).Except(seatIds).Single();
     }
 }
