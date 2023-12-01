6775a756b5c72d954d8afc5046302a3ead0df336
RobinTTY <muellerobin95@gmail.com>
2023-12-01 19:48:50 +0100

    Solve day 1

--- 2023/AdventOfCode/Day01.cs
+++ 2023/AdventOfCode/Day01.cs
@@ -1,15 +1,48 @@
-﻿namespace AdventOfCode;
+﻿using System.Globalization;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly IEnumerable<string> _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        var sum = _input.Sum(line => int.Parse(string.Concat(line.First(char.IsDigit), line.Last(char.IsDigit))));
+        return new (sum.ToString(CultureInfo.InvariantCulture));
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        string[] validNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+        var cleanInput = new List<string>();
+        
+        _input.ToList().ForEach(line =>
+        {
+            var index = 0;
+            var indexOfNumbersInText = new int?[9];
+            
+            foreach (var validNumber in validNumbers)
+            {
+                var result = line.IndexOf(validNumber, StringComparison.Ordinal);
+                indexOfNumbersInText[index] = result == -1 ? null : result;
+                index++;
+            }
+
+            var newLine = line;
+            var min = Array.FindIndex(indexOfNumbersInText, i => i == indexOfNumbersInText.Min());
+            var max = Array.FindIndex(indexOfNumbersInText, i => i == indexOfNumbersInText.Max());
+            newLine = newLine.Replace(validNumbers[min], (min + 1).ToString());
+            newLine = newLine.Replace(validNumbers[max], (max + 1).ToString());
+            cleanInput.Add(newLine);
+        });
+        
+        var sum = cleanInput.Sum(line => int.Parse(string.Concat(line.First(char.IsDigit), line.Last(char.IsDigit))));
+        return new (sum.ToString(CultureInfo.InvariantCulture));
+    }
 }
